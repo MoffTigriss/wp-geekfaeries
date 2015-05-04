28d3fc0fab6e9068fc5ca89c9b67f852b04c4d93
Moff Tigriss <moff.tigriss@gmail.com>
2015-05-04 20:10:09 +0200

    Cleaning code

--- header.php
+++ header.php
@@ -69,29 +69,25 @@
 
 			</header>
             
-            					<nav role="navigation" id="top-nav-editions" itemscope itemtype="http://schema.org/SiteNavigationElement">
-						<?php wp_nav_menu(array(
-    					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-    					         'menu' => __( 'Cartouches éditions à venir', 'wp-geekfaeries' ),  // nav name
-    					         'menu_class' => 'nav-editions top-nav cf',               // adding custom nav class
-    					         'theme_location' => 'editions',                 // where it's located in the theme
-    					         'before' => '',                                 // before the menu
-        			               'after' => '',                                  // after the menu
-        			               'link_before' => '',                            // before each link
-        			               'link_after' => '',                             // after each link
-        			               'depth' => 0,                                   // limit the depth of the nav
-    					         'fallback_cb' => '',                             // fallback function (if there is one)
-                                'reverse' => false
-						)); ?>
+            <nav role="navigation" id="top-nav-editions" itemscope itemtype="http://schema.org/SiteNavigationElement">
+                <?php wp_nav_menu(array(
+                         'container' => false,                           // remove nav container
+                         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+                         'menu' => __( 'Cartouches éditions à venir', 'wp-geekfaeries' ),  // nav name
+                         'menu_class' => 'nav-editions top-nav cf',               // adding custom nav class
+                         'theme_location' => 'editions',                 // where it's located in the theme
+                         'before' => '',                                 // before the menu
+                           'after' => '',                                  // after the menu
+                           'link_before' => '',                            // before each link
+                           'link_after' => '',                             // after each link
+                           'depth' => 0,                                   // limit the depth of the nav
+                         'fallback_cb' => '',                             // fallback function (if there is one)
+                        'reverse' => false
+                )); ?>
 
-					</nav>
+            </nav>
             
             <div id="banniere-cont">
 				<a href="<?php bloginfo('url'); ?>/"><img id="banniere-logo" src="<?php echo get_template_directory_uri();?>/library/images/2015_logo-gf.png" title="Geek Faëries" alt="Logo Geek Faëries"></a>
 				<a href="<?php bloginfo('url'); ?>/"><div id="banniere-image"></div></a>
-				<div id="row-cartouche-header" class="row" style="display:none">
-					<div id="rih-1" class="cartouche-header small-2 columns small-offset-7"><p><a href="/qui-sommes-nous/les-geek-faeries-cest-quoi/">I R L</a></p>V6.0 - 5 au 7 juin 2015<br /><i>Selles-sur-Cher (41)</i><br /><a href="/infos-pratiques/">Les Infos Pratiques</a></div>
-					<div id="rih-2" class="cartouche-header small-2 columns small-offset-1"><p><a href="/qui-sommes-nous/geek-faeries-ontheweb-cest-quoi/">On The Web</a></p>V4.0 - 15 au 17 janvier 2016<br />Gratuit & en streaming<br /><a href="/ontheweb-infos-pratiques/">Les Infos Pratiques</a></div>
-				</div>
 			</div>
