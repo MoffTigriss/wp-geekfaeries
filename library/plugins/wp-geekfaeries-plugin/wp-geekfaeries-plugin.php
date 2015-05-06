77e398179acd95fb00940d1a8855be8db563a56c
Moff Tigriss <moff.tigriss@gmail.com>
2015-05-06 04:00:08 +0200

    Adding base for specific plugin

---
+++ library/plugins/wp-geekfaeries-plugin/wp-geekfaeries-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Site Plugin for geekfaeries.fr
+Description: Site specific code changes for geekfaeries.fr
+*/
+/* Start Adding Functions Below this Line */
+
+
+
+/* Stop Adding Functions Below this Line */
+?>
